3f5e95a6cacad13ae7df952426012e1e9b782c67
Ghebrehiywet Fissaha <g.hiwetmit@gmail.com>
2019-05-31 13:42:56 +0300

    Add project files.

---
+++ RBMTEngAmh/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RBMTEngAmh.MvcApplication" Language="C#" %>
---
+++ RBMTEngAmh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RBMTEngAmh.Startup))]
+namespace RBMTEngAmh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
